5d2a2c2d7b71e39645e15a3103087deb83013180
Your Name <you@example.com>
2023-03-25 15:37:53 +0800

    更新添加合并

--- nonebot_plugin_warframe_mode/__init__.py
+++ nonebot_plugin_warframe_mode/__init__.py
@@ -1,8 +1,11 @@
 #本指令api均来自：https://github.com/WsureWarframe/warframe-info-api
 #群号435021808
 #这边为消息发送除了wm/rm/tran(翻译）这三个因为要获取信息所以在这边写
+
+import urllib.parse   #url编码
 from nonebot import on_command
 from nonebot.internal.adapter import Message
+from nonebot.adapters.onebot.v11 import Bot,GroupMessageEvent
 from nonebot.params import CommandArg
 from httpx import AsyncClient
 
@@ -11,19 +14,44 @@ from .data_source import classify,directives
 warframe = on_command("warframe", aliases={"", "/"},priority=5)
 
 @warframe.handle()
-async def receive(args: Message = CommandArg()):
+async def receive(bot:Bot, event: GroupMessageEvent,args: Message = CommandArg()):
     msg = await classify(args.extract_plain_text())
-    await warframe.finish(message=msg)
+
+    msg_list = []
+    msg_list.append(
+        {
+            "type": "node",
+            "data": {
+                "name": "这里是WFBOT",
+                "uin": event.self_id,
+                "content": msg
+            }
+        }
+    )
+    await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=msg_list)
+    #await warframe.finish(message=msg)
 
 
 #wm市场
 wm = on_command('wm',aliases={'wm','/wm'},priority=5,block=True)
 @wm.handle()
-async def _(msg: Message = CommandArg()):
+async def _(bot:Bot, event: GroupMessageEvent,msg: Message = CommandArg()):
     txt = msg.extract_plain_text().strip()
     url = f'http://nymph.rbq.life:3000/wm/robot/{txt}'
     message = await wm_api(url)
-    await wm.finish(message)
+    wm_msg_list = []
+    wm_msg_list.append(
+        {
+            "type": "node",
+            "data": {
+                "name": "这里是WFBOT",
+                "uin": event.self_id,
+                "content": f"本次查询：{txt}\n"+message
+            }
+        }
+    )
+    await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=wm_msg_list)
+    #await wm.finish(message)
 
 async def wm_api(url):
     async with AsyncClient() as client:
@@ -34,11 +62,23 @@ async def wm_api(url):
 #rm紫卡
 rm = on_command('rm',aliases={'rm','/rm'},priority=5,block=True)
 @rm.handle()
-async def _(msg: Message = CommandArg()):
+async def _(bot:Bot, event: GroupMessageEvent,msg: Message = CommandArg()):
     rm_txt = msg.extract_plain_text().strip()
     rm_url = f'http://nymph.rbq.life:3000/rm/robot/{rm_txt}'
     message_rm = await rm_api(rm_url)
-    await rm.finish(message_rm)
+    rm_msg_list = []
+    rm_msg_list.append(
+        {
+            "type": "node",
+            "data": {
+                "name": "这里是WFBOT",
+                "uin": event.self_id,
+                "content": f"本次查询：{rm_txt}\n"+message_rm
+            }
+        }
+    )
+    await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=rm_msg_list)
+    #await rm.finish(message_rm)
 
 async def rm_api(rm_url):
     async with AsyncClient() as client:
@@ -49,13 +89,37 @@ async def rm_api(rm_url):
 #中英翻译
 tran = on_command('tran',aliases={'tran','翻译'},priority=5,block=True)
 @tran.handle()
-async def _(msg: Message = CommandArg()):
+async def _(bot:Bot, event: GroupMessageEvent,msg: Message = CommandArg()):
     tran_txt = msg.extract_plain_text().strip()
     tran_url = f'http://nymph.rbq.life:3000/dict/tran/robot/{tran_txt}'
     message_tran = await tran_api(tran_url)
-    await tran.finish(message_tran)
+    tran_msg_list = []
+    tran_msg_list.append(
+        {
+            "type": "node",
+            "data": {
+                "name": "这里是WFBOT",
+                "uin": event.self_id,
+                "content": f"本次翻译：{tran_txt}\n"+message_tran
+            }
+        }
+    )
+    await bot.call_api('send_group_forward_msg', group_id=event.group_id, messages=tran_msg_list)
+    #await tran.finish(message_tran)
 
 async def tran_api(tran_url):
     async with AsyncClient() as client:
         res_tran = (await client.get(tran_url))
         return res_tran.text
+
+
+#wiki
+wiki = on_command('wiki', aliases={'wiki','维基'}, block=True)
+@wiki.handle()
+async def _(msg: Message = CommandArg()):
+    txt = msg.extract_plain_text().strip()
+    new_txt = urllib.parse.quote(txt)
+    url = f'这是您找的{txt}\n请复制到浏览器打开!\n\nhttps://warframe.huijiwiki.com/wiki/{new_txt}'
+    await wiki.finish(message=url)
+
+
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setuptools.setup(
     name = "nonebot_plugin_warframe_mode", ##模块名字
-    version = "0.0.3", ##版本号
+    version = "0.0.4", ##版本号
     author="mmxd12", ##作者名字
     author_email="mys0627@outlook.com", ##作者邮箱
     description="""基于nonebot2的WM市场查询的插件""", ##模块简介
@@ -14,13 +14,13 @@ setuptools.setup(
     url="https://github.com/mmxd12/nonebot_plugin_warframe_mode", ##模块链接
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.11",  # 使用Python3.11
+        "Programming Language :: Python :: 3.11",  # 使用Python3.11,建议使用3.9以上
         "License :: OSI Approved :: MIT License",  # 开源协议
         "Operating System :: OS Independent",
     ],
     install_requires=[
         'nonebot2>=2.0.0rc3',
-        'nonebot-adapter-onebot>=2.1.1',
+        'nonebot-adapter-onebot>=2.2.1',
         'httpx>=0.23.3',
     ],
 )
